7b5cdc337e30136d7c6ce441d2d67028e718c50b
Kristin Rievley <krievley@gmail.com>
2018-12-08 14:21:47 -0500

    Add initial templates and framework.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>HSCI E 128 Final</title>
+    <link rel="stylesheet" href="{!! asset('/css/app.css') !!}"/>
+    @yield('head')
+</head>
+<body>
+    @include('partials.nav')
+
+    <div id="app">
+        <div class="container">
+            @yield('content')
+        </div>
+    </div>
+
+    <script type="text/javascript" src="{!! asset('/js/app.js') !!}"></script>
+    @yield('footer-scripts')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>\
-    </head>
-    <body>
-        <ul class="nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
-    </body>
-</html>
+@extends('layouts.main')
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
