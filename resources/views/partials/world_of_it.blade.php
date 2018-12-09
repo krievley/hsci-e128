8347496d2ff08f8ad9b605b510a3319c25162be6
Kristin Rievley <krievley@gmail.com>
2018-12-09 15:16:33 -0500

    Added world of it blurb.

---
+++ resources/views/partials/world_of_it.blade.php
@@ -0,0 +1,6 @@
+<h2>Want to know more about World of I.T.?</h2>
+<p>
+    World of I.T. is a company that loves to explore the newest innovations in technology. The content they create
+    for this site is meant to demonstrate some possible problem solving solutions that technology innovations can
+    provide.
+</p>
