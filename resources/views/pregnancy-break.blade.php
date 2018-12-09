1c1aeff1daed0ffaeeb0570f080a989f136c4f1e
Kristin Rievley <krievley@gmail.com>
2018-12-09 14:08:05 -0500

    Created the pregnancy break page.

--- resources/views/pregnancy-break.blade.php
+++ resources/views/pregnancy-break.blade.php
@@ -20,10 +20,17 @@
                 </div>
             </div>
             <p class="lead">
-
+                Pregnancy can have a lot of undesirable side effects. For example, you cannot eat certain foods, drink
+                alcohol, or wear some of your favorite clothes. While motherhood is a blessing, and most women find
+                these disadvantages well worth the while, it can still be nice to take a break and enjoy some of the
+                things forbidden by pregnancy. This package was created with that concept in mind. Pregnant women can
+                download this vacation and indulge in the things that they've been missing without any harm to the baby.
             </p>
             <p class="lead">
-
+                Imagine yourself sipping a margarita on the beach, sporting you favorite swimsuit. This package comes
+                with the ability to connect to your friends in a virtual world so that you can put on your
+                favorite outfit and have a night out with your friends. Pregnancy is a beautiful gift, and you should
+                be able to enjoy every aspect of it while still taking time for yourself.
             </p>
             <p class="lead">
                 Try this experience today! It's fun and easy.
